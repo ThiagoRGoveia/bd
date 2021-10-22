864ec15c5d2160e7c2762d14c4e59cc0b1289f2c
Thiago <t.r.goveia@gmail.com>
2021-10-21 22:18:33 -0300

    Adiciona ex 1 e 2

---
+++ sql/lista5/ex1.sql
@@ -0,0 +1,18 @@
+
+DROP TRIGGER check_matricula ON Matricula;
+
+CREATE OR REPLACE FUNCTION check_matricula() RETURNS trigger AS $check_matricula$
+DECLARE
+quanti_matriculas INTEGER;
+BEGIN
+SELECT COUNT(*) INTO quanti_matriculas WHERE Aluno = NEW.Aluno
+FROM Matricula;
+IF quanti_matriculas > 20 THEN
+RAISE EXCEPTION 'Nao pode cadastrar mais disciplinas para esse aluno';
+END IF;
+RETURN NEW;
+END;
+$check_matricula$ LANGUAGE plpgsql;
+CREATE TRIGGER check_matricula
+BEFORE INSERT ON Matricula
+FOR EACH ROW EXECUTE PROCEDURE check_matricula();
---
+++ sql/lista5/ex2.sql
@@ -0,0 +1,25 @@
+-- Implemente uma regra de negócio que impede que professores tenham sua titulação atualizada para um status anterior segundo a
+-- ordem < Mestre, Doutor, Livre-docente, Titular>. Por exemplo, se a titulação de um dado professor for atualizada para Mestre,
+-- sendo que sua titulação anterior é Doutor ou acima, levante uma exceção impedindo a operação.
+
+
+DROP TRIGGER check_titulo_professor ON Matricula;
+
+CREATE OR REPLACE FUNCTION check_titulo_professor() RETURNS trigger AS $check_titulo_professor$
+DECLARE
+titulo_prof INTEGER;
+BEGIN
+SELECT Titulação INTO titulo_prof WHERE NFunc = NEW.NFunc
+FROM Matricula;
+IF NEW.Titulação = 'Titular' THEN RETURN NEW;
+IF NEW.Titulação = 'Livre-docente' AND Titulação != 'Titular' THEN RETURN NEW;
+IF NEW.Titulação = 'Doutor' AND Titulação != 'Livre-docente' AND Titulação != 'Titular' THEN RETURN NEW;
+-- Mestre não pode ser um valor inserido no UPDATE pois viola a regra de negócio
+END IF;
+RAISE EXCEPTION 'Nao pode regredir titulação';
+
+END;
+$check_titulo_professor$ LANGUAGE plpgsql;
+CREATE TRIGGER check_titulo_professor
+BEFORE UPDATE ON Professor
+FOR EACH ROW EXECUTE PROCEDURE check_titulo_professor();
