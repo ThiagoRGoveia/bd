c9d43bdb96a1ab09ab5441761a121d9c72f6985c
Matheus <matheusode@gmail.com>
2021-11-02 08:35:27 -0300

    removed some unnescessary stuff

--- sql/lista6/exercicio1_itens_f_ao_i.sql
+++ sql/lista6/exercicio1_itens_f_ao_i.sql
@@ -14,10 +14,15 @@ T3 <- pi (f_id, p_id) (fornecedores * catalogo)
 T4 <- T3 dividido por T1
 T5 <- T3 dividido por T2
 T6 <- T4 UNION T5
+T7 <- listar(f_id) (T6)
 */
 
 --item h
 /*
+T1 <- pi (p_id, p_nome, preco) (pecas)
+T2 <- pi (f_id, f_nome, p_id) (fornecedores)
+T3 <- (T2 NATURAL_JOIN (T1))
+T4 <- listar(f_id, f_nome, p_nome, preco) (T3)
 */
 
 --item i
